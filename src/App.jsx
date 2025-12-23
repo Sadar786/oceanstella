9beaa9ff99b863d6fecc76981210c41b22104efc
sadar786 <bathoorsadarullah@gmail.com>
2025-12-23 13:26:09 +0500

    mainlayout remane

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./layouts/minLayout";
+import MainLayout from "./layouts/MainLayout";
 
 import ScrollToTop from "./components/ScrollToTop";
 import RequireAuth from "./components/RequireAuth";

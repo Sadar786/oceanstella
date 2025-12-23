675fc51568fc50674cd55db9dfde3421432dc2a0
sadar786 <bathoorsadarullah@gmail.com>
2025-12-23 13:21:54 +0500

    mainLayoput name changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
+import MainLayout from "./layouts/minLayout";
 
 import ScrollToTop from "./components/ScrollToTop";
 import RequireAuth from "./components/RequireAuth";
